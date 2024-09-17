faac961ee921e329aaf010a8fcd0fc078e2f1015
caru-ini <caru17122@gmail.com>
2024-09-17 04:42:49 +0000

    feat: include lowercase characters in generateUniqueId

--- src/utils/generateUniqueId.ts
+++ src/utils/generateUniqueId.ts
@@ -1,5 +1,5 @@
 export const generateUniqueId = (length: number = 6): string => {
-  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
   let result = '';
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * characters.length));
