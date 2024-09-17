8e6a3df5751111e0715dfb9a9eb31f11358a9816
caru-ini <caru17122@gmail.com>
2024-09-17 04:42:10 +0000

    feat: update database schema

---
+++ prisma/migrations/20240917044145_/migration.sql
@@ -0,0 +1,40 @@
+-- CreateTable
+CREATE TABLE "FileInfo" (
+    "id" UUID NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "filePath" TEXT NOT NULL,
+    "fileSize" INTEGER NOT NULL,
+    "mimeType" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "FileInfo_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Share" (
+    "id" UUID NOT NULL,
+    "shareLink" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Share_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ShareFileInfo" (
+    "shareId" UUID NOT NULL,
+    "fileInfoId" UUID NOT NULL,
+
+    CONSTRAINT "ShareFileInfo_pkey" PRIMARY KEY ("shareId","fileInfoId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Share_shareLink_key" ON "Share"("shareLink");
+
+-- AddForeignKey
+ALTER TABLE "ShareFileInfo" ADD CONSTRAINT "ShareFileInfo_shareId_fkey" FOREIGN KEY ("shareId") REFERENCES "Share"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ShareFileInfo" ADD CONSTRAINT "ShareFileInfo_fileInfoId_fkey" FOREIGN KEY ("fileInfoId") REFERENCES "FileInfo"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
