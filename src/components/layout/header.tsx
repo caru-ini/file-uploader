7bca1481303bfee5cebe03ca1d80a508c789edba
caru-ini <caru17122@gmail.com>
2024-09-18 09:01:30 +0000

    feat: update bg transparency

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -4,8 +4,8 @@ import UserDropdownMenu from './user-menu';
 
 export const Header: React.FC = () => {
   return (
-    <header className='sticky top-0 z-50 flex grow-0 select-none items-center justify-between border-b border-border bg-background/40 p-3 backdrop-blur-md'>
-      <Link href={'/'} className='flex items-center'>
+    <header className='sticky top-0 z-50 flex grow-0 select-none items-center justify-between border-b border-border bg-background/80 p-3 backdrop-blur-md'>
+      <Link href={'/'} className='flex items-center text-lg'>
         <Folder className='mr-2 fill-yellow-300 stroke-muted-foreground' size={24} />
         File Uploader
       </Link>
